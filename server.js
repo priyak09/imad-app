eb52fc5683b2069c9ce93c3a5a96d7f1fb27fe89
priyak09 <30681820+priyak09@users.noreply.github.com>
2017-08-16 11:31:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -27,7 +27,8 @@ function createTemplate(data)
     var heading = data.heading;
     var date = data.date;
     var content= data.content;
-    var htmlTemplate: `<html>
+    var htmlTemplate = `
+    <html>
     <head>
         <title>${title}</title>
         <meta name = "viewport" content="width=device-width, initial-scale=1"/>
@@ -71,7 +72,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.send(createTemplate(articleOne);
+  res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two', function (req, res) {
