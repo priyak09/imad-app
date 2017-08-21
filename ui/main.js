b52eeedb85a78d6e2d92017473468fc90c4dcd9f
priyak09 <30681820+priyak09@users.noreply.github.com>
2017-08-21 16:03:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,15 @@
-console.log('Loaded!');
-
-var element = document.getElementById('main-text');
-element.innerHTML = 'new value';
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight(){
-    marginLeft = marginLeft +10;
-    img.style.marginLeft = marginLeft + 'px';
-}; 
-img.onclick = function(){
-  var interval = setInterval(moveRight,50);  
+var button = document.getElementById('counter');
+button.onclick = function(){
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function(){
+      if(request.readyState === XMLHttprequest.DONE){
+          if(request.status === 200){
+          var counter = request.responseText;
+          var span = document.getElementById('count');
+          span.innerHTML = counter.toString();
+          }
+      }
+  };
+  request.open('GET','http://priyak.twinkle.imad.hasura.io/counter'.true);
+  request.send(null);
 };
